cd434db09e44959f8ec2f5bc468f8176596c15a9
Nesy Widyastuti <56398122+NesyWidya24@users.noreply.github.com>
2021-03-31 23:58:24 +0700

    Initial Commit

--- app/src/main/java/com/kagu/warehouseapp/StartActivity.java
+++ app/src/main/java/com/kagu/warehouseapp/StartActivity.java
@@ -2,13 +2,33 @@ package com.kagu.warehouseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class StartActivity extends AppCompatActivity {
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        if (getSupportActionBar()!=null)
+            getSupportActionBar().hide();
+
+        Window w = getWindow();
+        //mengatur tampilan awal menjadi fullscreen
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+                StartActivity.this.finish();
+            }
+        }, 3000L);
     }
 }
